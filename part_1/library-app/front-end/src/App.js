dd53d90bc779b3e30a44f5595e4a0380083b06b1
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-09-05 21:09:33 +0300

    part 1 sodoCode for frontend

--- part_1/library-app/front-end/src/App.js
+++ part_1/library-app/front-end/src/App.js
@@ -1,6 +1,61 @@
 import React from "react";
 import "./App.css";
 
+/*
+
+بنعمل راورتات بعد من نعمل بلايندكس براوزر بنيجي بنعمل رواترات هون وبنفقيشين كومبونات بنعمل لينك تو 
+
+*/
+/*
+
+Q6) Add a frontend route `/library/create_book` to render the inputs that correspond to 
+ the book's attributes and a `submit` button (without functionality).
+
+بدنا نعمل لبراوزر بعدين  كومبونت ونستدعي هون وفلكومبونت نعمل اكسيوس دوت كريات على الباث الموجود واسيف القيم بستاتيس هوك 
+بمتغيرات بعدين اعبيهم كلهم باوبجكت وارسلوا مع الاكسيوس عشان ينشأ واحد جديد 
+
+*/
+
+/*
+
+Q7) Add a frontend route `/library` that will render all the books (title and author) from the database.
+
+بنعمل راوتر جديد لباث تبعوا المكتوب 
+وبنحطلوا لينك عنفقيشن 
+بنروح علكومبونت تبعوا 
+منعمل اكيسوس دوت جيت 
+وجوا ذن بنحط رزيلت دوت داتا دوت العنوان وبنسيفهم بستيتاس هوك
+وكمان لاوثر 
+وبعرضهم عشاشة عن طريق اكتب اسمهم برتيرن
+
+*/
+
+/*
+Q8) Add a frontend route `/library/:book_id` that will render a specific book details from the database.
+
+
+بنعمل راوتر جديد لباث تبعوا المكتوب 
+وبنحطلوا لينك عنفقيشن 
+بنروح علكومبونت تبعوا 
+منعمل اكيسوس دوت جيت
+لباث الموجود وبكون الايدي معرف عندي عشان يروح عليه 
+فلباث بكون باك تيك 
+عشان اجيب معلوماتوا
+بعدين دت ريزلت وبريزلت بطلعلي قيمتوا بحطو بتغير ستيتاس هوك 
+وبستدعي برتيرن
+
+*/
+
+/*
+Q9) When the `submit` button is clicked (from Q6) it should add a new book in the database, by sending a request to the correct endpoint in the backend.
+بنحط بوتوم بنعمل عليها فنكسن اون كليك اسم لفنكسن بروح عليه بنعمل اكسيوس دوت كريات والابوجك لي انا اصلا عاملوا ببعتوا مع الاكسيوس 
+
+*/
+
+/*
+العاشر ما لحقت اقرئوا 
+الوقت 9:9
+*/
 const App = () => {
   return (
     <>
