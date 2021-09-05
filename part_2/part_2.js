e87e34e3c95dcce05115bea273f2d6827f1ed50b
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-09-05 20:55:26 +0300

    part 2 finshed

--- part_2/part_2.js
+++ part_2/part_2.js
@@ -6,31 +6,61 @@ Type the correct answer below each question:
 
 Q1) Why do we use databases?
 
+    عشان اقدر اعمل سيف لداتا لعندي واقدر ارجع استعملها واعمل عليها مختلف العمليات كاستدعاء وتعديل وحذف
 
 Q2) What are JWTs used for?.
+
+jsonWeb نسيت
+هاي مكتبة بنقدر من خلالها نعمل تحقق و دخول 
+sign
+veartifly
+وبتحدث توكين 
+ 
+بنحط فيها لبيلود والاوبشن لبدنا ياها وبعطينا توكين
 
 
 Q3) What are the main three sections of a JWT?
 
+secret 
+options
+payload
 
 Q4) Name three types of express middle-wares.
 
+لمدل وير تبع نكست بكون ريكويست وريس و نكست 
+
 
 Q5) What is the `effect` hook used for in react?
-``
+
+ هاي عبارة عن فنكشن بقدر استعملها او اشغلها اذا بدي اعمل ريلود المعلومات معينة حسب اشي معين او مره واحدة``
 
 Q6) What is the `state` hook used for in react?
 
+عبارة عن فنكسن بقدر اعرف فيه قيمة اولية واقدر اصير اغير عليها وتسيف فنفس اسم لقيمة الاولية 
 
 Q7) List three thing you can use to create a responsive web application.
 
+javsscript 
+mongoose
+react 
 
 Q8) Name two of mongoose middle-wares and explain what they are used for.
 
+pre
+post
 
 Q9) What is the difference between authentication and authorization?
 
+authentication
+تحقق من المعلومات  وعن طريقها
+
+authorization
+فحص صلاحيات لشخص واعطائها
 
 Q10) Why do we save some information as an environment variable (in .env file)?
 
+عشان بتكون فغاية لسرية وما بدنا نشاركها مع حدا زي 
+secret
+url
+اشياء ما بدنا نشاركها بدنا ياها بس عجاهزنا 
 */
