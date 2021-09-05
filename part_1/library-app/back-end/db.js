0e55c0acfa177ca06676ae78525315d1234b7fe1
ObadaAmarneh <obada.amarneh20@gmail.com>
2021-09-05 19:57:40 +0300

    Q1 finshed and start in Q2

--- part_1/library-app/back-end/db.js
+++ part_1/library-app/back-end/db.js
@@ -1,1 +1,15 @@
 // db
+const mongoose = require("mongoose")
+
+const options = {
+    /// ضل يعطيني غلط مش عارفوا 
+    // useNewUrlParser = true,
+    // useCreateIndex = true ,
+    // useUnifiedTopology = true
+}
+
+mongoose.connect("mongodb://localhost:27017/exam" , options).then(()=>{
+    console.log(" --> connected <-- ");
+},(err)=>{
+    console.log(err);
+})
